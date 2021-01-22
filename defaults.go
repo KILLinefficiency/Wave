b831004382eebadf2f5f2c10c8336a128941b9ac
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-23 00:14:39 +0530

    added maps for default values

---
+++ defaults.go
@@ -0,0 +1,31 @@
+package main
+
+var pageDefaults = map[string]string {
+  "pTitle": "Wave Document",
+  "pBGcolor": "white",
+  "pBGimage": "none",
+  "pAlign": "left",
+  "pBox": "0",
+  "pBoxStyle": "hidden",
+}
+
+var contentDefaults = map[string]string {
+  "cTab": "4",
+  "cFont": "Arial",
+  "cSize": "17",
+  "cColor": "black",
+  "cBox": "0",
+  "cBoxStyle": "hidden",
+  "cAlign": "",
+  "cBGcolor": "",
+  "cPointsType": "ul",
+  "cPointsStyle": "disc",
+  "cWidth": "none",
+  "cHeight": "none",
+  "cDelimiter": ";",
+  "cTableDelimiter": "|",
+  "cLink": "https://www.github.com/KILLinefficiency/Wave",
+  "cMailTitle": "",
+  "cMailAddress": "",
+  "cLinkTitle": "Wave",
+}
