f7c1234ff6d9739dcf54d6d52008afa14ff9222d
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-23 00:41:27 +0530

    added a function to copy maps

--- lib.go
+++ lib.go
@@ -1,5 +1,11 @@
 package main
 
+func copyMap(mapOrignal map[string]string, mapCopy map[string]string) {
+  for key, value := range mapOrignal {
+    mapCopy[key] = value
+  }
+}
+
 func strMultiply(strText string, times int) string {
   var strFinal string
   for loop := 0; loop < times; loop = loop + 1 {
