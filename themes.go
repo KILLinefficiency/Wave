e56b9e6b2fdb66bf29286127797e48d55a4851b6
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-24 22:08:41 +0530

    added OneDark theme

---
+++ themes.go
@@ -0,0 +1,15 @@
+package main
+
+var themes = map[string]map[string]string {
+  "OneDark": {
+    "bg":        "#1E1F29",
+    "black":     "#282C34",
+    "white":     "#ABB2BF",
+    "red":       "#E06C75",
+    "green":     "#98C379",
+    "yellow":    "#D19A66",
+    "blue":      "#61AFEF",
+    "magenta":   "#C678DD",
+    "cyan":      "#56B6C2",
+  },
+}
