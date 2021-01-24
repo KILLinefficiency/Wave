18d1723a4de3cd51b800dc5ec2a928530f0ae141
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-25 03:42:37 +0530

    added html and css templates

---
+++ htmlTemplates.go
@@ -0,0 +1,14 @@
+package main
+
+var (
+
+  waveMark string = "<!--\nThis Document is generated using Wave.\nWave: https://www.github.com/KILLinefficiency/Wave\n-->\n\n"
+
+  htmlTopBodyTemplate string = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n"
+
+  htmlCSSTemplate string = "\t<body style = 'background-color: %s; background-image: %s; text-align: %s; margin: %spx; border-style: %s;'>\n"
+
+  cssTemplate string = "style = 'font-family: %s; color: %s; background-color: %s; font-size: %spx; text-align: %s; margin: %spx; border-style: %s; list-style-type: %s;'"
+
+  htmlEnd string = "\t</body>\n</html>"
+)
