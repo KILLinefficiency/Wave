61876c44c9cf01caf87c2912743eb27705604389
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-25 03:52:11 +0530

    made default work and changed the names of the page and content properties maps

--- wave.go
+++ wave.go
@@ -29,66 +29,70 @@ func main() {
 
   var script string = string(byteStream)
   scriptLines := strings.Split(strings.TrimSpace(script), "\n")
+
+  var contentDefaults = make(map[string]string)
+  copyMap(contentProp, contentDefaults)
+  
   for _, line := range scriptLines {
     tokens := strings.Split(strings.TrimSpace(line), " ")
     property := strings.Join(tokens[1:], " ")
 
     switch tokens[0] {
       case "~title":
-        pageDefaults["pTitle"] = property
+        pageProp["pTitle"] = property
       case "~bg":
-        pageDefaults["pBGcolor"] = property
-        contentDefaults["cBGcolor"] = pageDefaults["pBGcolor"]
+        pageProp["pBGcolor"] = property
+        contentProp["cBGcolor"] = pageProp["pBGcolor"]
       case "~pic":
-        pageDefaults["pBGimage"] = property
+        pageProp["pBGimage"] = property
       case "~align":
-        pageDefaults["pAlign"] = property
-        contentDefaults["cAlign"] = pageDefaults["pAlign"]
+        pageProp["pAlign"] = property
+        contentProp["cAlign"] = pageProp["pAlign"]
       case "~box":
-        pageDefaults["pBox"] = property
+        pageProp["pBox"] = property
       case "~box-style":
-        pageDefaults["pBoxStyle"] = property
+        pageProp["pBoxStyle"] = property
     }
 
     switch tokens[0] {
       case "!tab":
-        contentDefaults["cTab"] = property
+        contentProp["cTab"] = property
       case "!font":
-        contentDefaults["cFont"] = property
+        contentProp["cFont"] = property
       case "!size":
-        contentDefaults["cSize"] = property
+        contentProp["cSize"] = property
       case "!color":
-        contentDefaults["cColor"] = property
+        contentProp["cColor"] = property
       case "!box":
-        contentDefaults["cBox"] = property
+        contentProp["cBox"] = property
       case "!box-style":
-        contentDefaults["cBoxStyle"] = property
+        contentProp["cBoxStyle"] = property
       case "!align":
-        contentDefaults["cAlign"] = property
+        contentProp["cAlign"] = property
       case "!bg":
-        contentDefaults["cBGcolor"] = property
+        contentProp["cBGcolor"] = property
       case "!points-type":
         if property == "ordered" {
-          contentDefaults["cPointsType"] = "ol"
+          contentProp["cPointsType"] = "ol"
         }
         if property == "unordered" {
-          contentDefaults["cPointsType"] = "ul"
+          contentProp["cPointsType"] = "ul"
         }
       case "!points-style":
-        contentDefaults["cPointsStyle"] = property
+        contentProp["cPointsStyle"] = property
       case "!dim":
         widthHeight := strings.Split(property, "x")
-        contentDefaults["cWidth"] = strings.TrimSpace(widthHeight[0])
-        contentDefaults["cHeight"] = strings.TrimSpace(widthHeight[1])
+        contentProp["cWidth"] = strings.TrimSpace(widthHeight[0])
+        contentProp["cHeight"] = strings.TrimSpace(widthHeight[1])
       case "!sep":
-        contentDefaults["cDelimiter"] = property
+        contentProp["cDelimiter"] = property
       case "!colsep":
-        contentDefaults["cTableDelimiter"] = property
+        contentProp["cTableDelimiter"] = property
       case "!default":
-        fmt.Println("will do this later brrr...")
+        copyMap(contentDefaults, contentProp)
     }
 
-    cssBody = fmt.Sprintf("style = 'font-family: %s; color: %s; background-color: %s; font-size: %spx; text-align: %s; margin: %spx; border-style: %s; list-style-type: %s;'", contentDefaults["cFont"], contentDefaults["cColor"], contentDefaults["cBGcolor"], contentDefaults["cSize"], contentDefaults["cAlign"], contentDefaults["cBox"], contentDefaults["cBoxStyle"], contentDefaults["cPointsStyle"])
+    cssBody = fmt.Sprintf(cssTemplate, contentProp["cFont"], contentProp["cColor"], contentProp["cBGcolor"], contentProp["cSize"], contentProp["cAlign"], contentProp["cBox"], contentProp["cBoxStyle"], contentProp["cPointsStyle"])
 
     switch tokens[0] {
       case "$text":
@@ -96,7 +100,7 @@ func main() {
 
       case "$file":
         textFile, _ := ioutil.ReadFile(property)
-        tabNumber, _ := strconv.Atoi(contentDefaults["cTab"])
+        tabNumber, _ := strconv.Atoi(contentProp["cTab"])
         var fileStr string = string(textFile)
         fileStr = strings.Replace(fileStr, "\n", "<br>", -1)
         fileStr = strings.Replace(fileStr, " ", "&nbsp;", -1)
@@ -111,36 +115,36 @@ func main() {
         htmlBody += fmt.Sprintf("\t\t%s\n", strMultiply("<br>", times))
 
       case "$link":
-        linkTitle := strings.Split(property, contentDefaults["cDelimiter"])
+        linkTitle := strings.Split(property, contentProp["cDelimiter"])
         if len(linkTitle) > 1 {
-          contentDefaults["cLink"] = strings.TrimSpace(linkTitle[0])
-          contentDefaults["cLinkTitle"] = strings.TrimSpace(linkTitle[1])
+          contentProp["cLink"] = strings.TrimSpace(linkTitle[0])
+          contentProp["cLinkTitle"] = strings.TrimSpace(linkTitle[1])
         }
-        htmlBody += fmt.Sprintf("\t\t<a href = '%s' %s>%s</a>\n", contentDefaults["cLink"], cssBody, contentDefaults["cLinkTitle"])
+        htmlBody += fmt.Sprintf("\t\t<a href = '%s' %s>%s</a>\n", contentProp["cLink"], cssBody, contentProp["cLinkTitle"])
 
       case "$mail":
-        mailTitle := strings.Split(property, contentDefaults["cDelimiter"])
+        mailTitle := strings.Split(property, contentProp["cDelimiter"])
         if len(mailTitle) > 1 {
-          contentDefaults["cMailAddress"] = strings.TrimSpace(mailTitle[0])
-          contentDefaults["cMailTitle"] = strings.TrimSpace(mailTitle[1])
+          contentProp["cMailAddress"] = strings.TrimSpace(mailTitle[0])
+          contentProp["cMailTitle"] = strings.TrimSpace(mailTitle[1])
         }
-        htmlBody += fmt.Sprintf("\t\t<a href = 'mailto:%s' %s>%s</a>\n", contentDefaults["cMailAddress"], cssBody, contentDefaults["cMailTitle"])
+        htmlBody += fmt.Sprintf("\t\t<a href = 'mailto:%s' %s>%s</a>\n", contentProp["cMailAddress"], cssBody, contentProp["cMailTitle"])
 
       case "$points":
-        listPoints := strings.Split(property, contentDefaults["cDelimiter"])
+        listPoints := strings.Split(property, contentProp["cDelimiter"])
         var allPoints string
         for _, point := range listPoints {
           allPoints += fmt.Sprintf("\t\t\t<li>%s</li>\n", strings.TrimSpace(point))
         }
-        var pointsBody string = fmt.Sprintf("\t\t<%s %s>\n%s\t\t</%s>\n", contentDefaults["cPointsType"], cssBody, allPoints, contentDefaults["cPointsType"])
+        var pointsBody string = fmt.Sprintf("\t\t<%s %s>\n%s\t\t</%s>\n", contentProp["cPointsType"], cssBody, allPoints, contentProp["cPointsType"])
         htmlBody += pointsBody
 
       case "$table":
         var tableBody string
         var tableBorder string = "style = 'border: 2px solid black;'"
-        tableRows := strings.Split(property, contentDefaults["cTableDelimiter"])
+        tableRows := strings.Split(property, contentProp["cTableDelimiter"])
         for _, rowValues := range tableRows {
-          values := strings.Split(rowValues, contentDefaults["cDelimiter"])
+          values := strings.Split(rowValues, contentProp["cDelimiter"])
           var rowBody string
           for _, addValues := range values {
             rowBody += fmt.Sprintf("\t\t\t\t\t<td %s>%s</td>\n", tableBorder, strings.TrimSpace(addValues))
@@ -150,7 +154,7 @@ func main() {
         htmlBody += fmt.Sprintf("\t\t<div %s>\n\t\t\t<table %s>\n%s\t\t\t</table>\n\t\t</div>\n", cssBody, tableBorder, tableBody)
 
       case "$check":
-        checkPoints := strings.Split(property, contentDefaults["cDelimiter"])
+        checkPoints := strings.Split(property, contentProp["cDelimiter"])
         var checkPointsBody string
         for _, points := range checkPoints {
           checkPointsBody += fmt.Sprintf("\t\t\t<input type = 'checkbox'>%s<br>\n", strMultiply("&nbsp;", 2) + strings.TrimSpace(points))
@@ -161,7 +165,7 @@ func main() {
         htmlBody += fmt.Sprintf("\t\t<br><b><i>\"%s\"</b></i><br>\n", property)
 
       case "$pic":
-        htmlBody += fmt.Sprintf("\t\t<div style = 'text-align: %s; margin: %spx; border-style: %s;'>\n\t\t\t<img width = '%s' height = '%s' src = %s>\n\t\t</div>\n", contentDefaults["cAlign"], contentDefaults["cBox"], contentDefaults["cBoxStyle"],contentDefaults["cWidth"], contentDefaults["cHeight"], property)
+        htmlBody += fmt.Sprintf("\t\t<div style = 'text-align: %s; margin: %spx; border-style: %s;'>\n\t\t\t<img width = '%s' height = '%s' src = %s>\n\t\t</div>\n", contentProp["cAlign"], contentProp["cBox"], contentProp["cBoxStyle"],contentProp["cWidth"], contentProp["cHeight"], property)
 
       case "$html":
         htmlBody += fmt.Sprintf("\t\t%s\n", property)
@@ -169,10 +173,9 @@ func main() {
 
   }
 
-  var waveMark string = "\n<!--\nThis Document is generated using Wave.\nWave: https://www.github.com/KILLinefficiency/Wave\n-->\n\n"
-  var htmlTopBody string = fmt.Sprintf("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n", pageDefaults["pTitle"])
-  var htmlCSS string = fmt.Sprintf("\t<body style = 'background-color: %s; background-image: %s; text-align: %s; margin: %spx; border-style: %s;'>\n", pageDefaults["pBGcolor"], pageDefaults["pBGimage"], pageDefaults["pAlign"], pageDefaults["pBox"], pageDefaults["pBoxStyle"])
-  var htmlComplete string = waveMark + htmlTopBody + htmlCSS + htmlBody + "\t</body>\n</html>\n"
+  var htmlTopBody string = fmt.Sprintf(htmlTopBodyTemplate, pageProp["pTitle"])
+  var htmlCSS string = fmt.Sprintf(htmlCSSTemplate, pageProp["pBGcolor"], pageProp["pBGimage"], pageProp["pAlign"], pageProp["pBox"], pageProp["pBoxStyle"])
+  var htmlComplete string = waveMark + htmlTopBody + htmlCSS + htmlBody + htmlEnd
 
   fileName := strings.Split(sourceName, ".")
   if len(fileName) == 1 {
