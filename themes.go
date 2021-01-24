d3984c47145250294f03ed67e59e9e0abbeae8d4
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-24 22:36:21 +0530

    added Monokai theme and added some colors to other themes

--- themes.go
+++ themes.go
@@ -1,6 +1,7 @@
 package main
 
 var themes = map[string]map[string]string {
+
   "OneDark": {
     "bg":        "#1E1F29",
     "black":     "#282C34",
@@ -8,6 +9,7 @@ var themes = map[string]map[string]string {
     "red":       "#E06C75",
     "green":     "#98C379",
     "yellow":    "#D19A66",
+    "orange":    "#D19A66",
     "blue":      "#61AFEF",
     "magenta":   "#C678DD",
     "cyan":      "#56B6C2",
@@ -20,8 +22,21 @@ var themes = map[string]map[string]string {
     "red":       "#FF5555",
     "green":     "#50FA7B",
     "yellow":    "#F1FA8C",
+    "orange":    "#FFB86C",
     "blue":      "#BD93F9",
     "magenta":   "#FF79C6",
     "cyan":      "#8BE9FD",
+  },
+
+  "Monokai": {
+    "bg":        "#272822",
+    "black":     "#000000",
+    "white":     "#F8F8F2",
+    "red":       "#F92672",
+    "green":     "#A6E22E",
+    "yellow":    "#E6DB74",
+    "orange":    "#FD971F",
+    "blue":      "#66D9EF",
+    "purple":    "#AE81FF",
   },
 }
