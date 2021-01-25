d4e5be4708aa64dea89c1127b74d840f76a8e67d
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-25 20:52:58 +0530

    added a css element

--- htmlTemplates.go
+++ htmlTemplates.go
@@ -6,7 +6,7 @@ var (
 
   htmlTopBodyTemplate string = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n"
 
-  htmlCSSTemplate string = "\t<body style = 'background-color: %s; background-image: %s; text-align: %s; margin: %spx; border-style: %s;'>\n"
+  htmlCSSTemplate string = "\t<body style = 'background-color: %s; background-image: %s; color: %s; text-align: %s; margin: %spx; border-style: %s;'>\n"
 
   cssTemplate string = "style = 'font-family: %s; color: %s; background-color: %s; font-size: %spx; text-align: %s; margin: %spx; border-style: %s; list-style-type: %s;'"
 
