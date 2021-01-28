773720452a73f1265a3beddeea6e2b259f39d14b
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-01-28 18:21:21 +0530

    used map for the templates

--- htmlTemplates.go
+++ htmlTemplates.go
@@ -1,41 +1,41 @@
 package main
 
-var (
+var templates = map[string]string {
 
-  waveMark string = "<!--\nThis Document is generated using Wave.\nWave: https://www.github.com/KILLinefficiency/Wave\n-->\n\n"
+  "waveMark": "<!--\nThis Document is generated using Wave.\nWave: https://www.github.com/KILLinefficiency/Wave\n-->\n\n",
 
-  htmlTopBodyTemplate string = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n"
+  "htmlTopBody": "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>%s</title>\n\t</head>\n",
 
-  htmlCSSTemplate string = "\t<body style = 'background-color: %s; background-image: %s; color: %s; text-align: %s; margin: %spx; border-style: %s;'>\n"
+  "htmlCSS": "\t<body style = 'background-color: %s; background-image: %s; color: %s; text-align: %s; margin: %spx; border-style: %s;'>\n",
 
-  cssTemplate string = "style = 'font-family: %s; color: %s; background-color: %s; font-size: %spx; text-align: %s; margin: %spx; border-style: %s; list-style-type: %s;'"
+  "css": "style = 'font-family: %s; color: %s; background-color: %s; font-size: %spx; text-align: %s; margin: %spx; border-style: %s; list-style-type: %s;'",
 
-  textTemplate string = "\t\t<p %s>%s</p>\n"
+  "text": "\t\t<p %s>%s</p>\n",
 
-  linkTemplate string = "\t\t<a href = '%s' %s>%s</a>\n"
+  "link": "\t\t<a href = '%s' %s>%s</a>\n",
 
-  mailTemplate string = "\t\t<a href = 'mailto:%s' %s>%s</a>\n"
+  "mail": "\t\t<a href = 'mailto:%s' %s>%s</a>\n",
 
-  pointsTemplate string = "\t\t\t<li>%s</li>\n"
+  "points": "\t\t\t<li>%s</li>\n",
 
-  pointsBodyTemplate string = "\t\t<%s %s>\n%s\t\t</%s>\n"
+  "pointsBody": "\t\t<%s %s>\n%s\t\t</%s>\n",
 
-  tableBorder string = "style = 'border: 2px solid black;'"
+  "tableBorder": "style = 'border: 2px solid black;'",
 
-  tableValuesTemplate string = "\t\t\t\t\t<td %s>%s</td>\n"
+  "tableValues": "\t\t\t\t\t<td %s>%s</td>\n",
 
-  tableBodyTemplate string = "\t\t\t\t<tr %s>\n%s\t\t\t\t</tr>\n"
+  "tableBody": "\t\t\t\t<tr %s>\n%s\t\t\t\t</tr>\n",
 
-  tableCompleteTemplate string = "\t\t<div %s>\n\t\t\t<table %s>\n%s\t\t\t</table>\n\t\t</div>\n"
+  "tableComplete": "\t\t<div %s>\n\t\t\t<table %s>\n%s\t\t\t</table>\n\t\t</div>\n",
 
-  checkboxTemplate string = "\t\t\t<input type = 'checkbox'>%s<br>\n"
+  "checkbox": "\t\t\t<input type = 'checkbox'>%s<br>\n",
 
-  checkboxBodyTemplate string = "\t\t<div %s>\n%s\t\t</div>\n"
+  "checkboxBody": "\t\t<div %s>\n%s\t\t</div>\n",
 
-  quoteTemplate string = "\t\t<br><b><i>\"%s\"</b></i><br>\n"
+  "quote": "\t\t<br><b><i>\"%s\"</b></i><br>\n",
 
-  imageTemplate string = "\t\t<div style = 'text-align: %s; margin: %spx; border-style: %s;'>\n\t\t\t<img width = '%s' height = '%s' src = %s>\n\t\t</div>\n"
+  "image": "\t\t<div style = 'text-align: %s; margin: %spx; border-style: %s;'>\n\t\t\t<img width = '%s' height = '%s' src = %s>\n\t\t</div>\n",
 
-  htmlEnd string = "\t</body>\n</html>"
+  "htmlEnd": "\t</body>\n</html>",
 
-)
+}
