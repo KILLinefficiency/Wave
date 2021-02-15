10c8edb753a998fd2e1e693fc59bdf441ebbf852
KILLinefficiency <shreyas.sable2166@gmail.com>
2021-02-15 14:52:42 +0530

    added dew special variables

--- variables.go
+++ variables.go
@@ -3,4 +3,10 @@ package main
 var variables = map[string]string {
     "source_name": getSourceName(),
     "file_name": makeHTML(getSourceName()),
+    "sp": "&nbsp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    "&": "&amp;",
+    "\"": "&quot;",
+    "'": "&apos;",
 }
